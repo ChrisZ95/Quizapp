960dafeff2e39dff8ebe3ed5b9b5a41edb575bea
Christopher Zwick <christopherzwick@Christophers-MacBook-Pro.local>
2024-02-20 08:48:11 +0100

    QuizApp done

--- script.js
+++ script.js
@@ -53,11 +53,14 @@ let questions = [
         "answer_2": "100 = let rate;",
         "answer_3": "rate = 100;",
         "answer_4": "let rate = 100;",
-        "right-answer": 4
+        "right_answer": 4
       }
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length
@@ -66,26 +69,70 @@ function init() {
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
+    if(gameIsOver()) {
+      showEndScreen(); 
+    } else {
+      updateToNextQuestion();
+    }
+}
 
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+function gameIsOver(){
+  return currentQuestion >= questions.length;
 }
 
 function answer(selection){
     let question = questions[currentQuestion];
-    console.log('Selected answer is ', selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQuestionNumber is ', selectedQuestionNumber);
-    console.log('Current question is ', question['right_answer']);
+    let idofRightAnswer = `answer_${question['right_answer']}`;
 
-    if(selectedQuestionNumber == question['right_answer']) {
-        console.log('Richtige Antwort')
+    if(rightAnswerSelected(selectedQuestionNumber, question)) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
+        rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idofRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
+    document.getElementById('next-button').disabled = false;
+}
+
+function rightAnswerSelected(selectedQuestionNumber, question) {
+  return selectedQuestionNumber == question['right_answer'];
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  document.getElementById('next-button').disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+}
+
+function resetAnswerButtons() {
+  document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function showEndScreen() {
+  document.getElementById('endScreen').style = '';
+  document.getElementById('questionBody').style = 'display: none';
+  document.getElementById('amount-of-questions').innerHTML = questions.length;
+  document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+}
+
+function updateToNextQuestion() {
+  let question = questions[currentQuestion];
+
+  document.getElementById('question-number').innerHTML = currentQuestion+1
+  document.getElementById('questiontext').innerHTML = question['question'];
+  document.getElementById('answer_1').innerHTML = `<span class="letter">A</span> ${question['answer_1']}`;
+  document.getElementById('answer_2').innerHTML = `<span class="letter">B</span> ${question['answer_2']}`;
+  document.getElementById('answer_3').innerHTML = `<span class="letter">C</span> ${question['answer_3']}`;
+  document.getElementById('answer_4').innerHTML = `<span class="letter">D</span> ${question['answer_4']}`;
 }
